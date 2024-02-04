65d347ba627a36e0d0b6600452e1017dd52fa6b5
Siddhartha Vibhu Pharswan <{ID}+{username}@users.noreply.github.com>
2024-02-04 23:04:34 +0530

    changes for env

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 function App() {
 
+  console.log(process.env.REACT_APP_BACKEND_URL);
   const [error, setError] = useState(null);
   const [data, setData] = useState([]);
 
